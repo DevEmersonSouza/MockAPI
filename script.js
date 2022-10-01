0c1c2ae973cacfae6fb49b045ce1c10ef3beee01
Emerson Souza <souzaemerson003@gmail.com>
2022-10-01 17:11:44 -0300

    atividade de classe - resolvida

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-function executar(cavalo){
-    return cavalo.json();
+function executar(cavalo) {
+    let promessabody = cavalo.json();
+    return promessabody
 }
 
-function executar2(jumento){
-    console.log(jumento)
-    document.write("<span>"+jumento[0].idade+"<span>")
-    lista.innerHTML = "<li>allan2<li>"
+function executar2(jumento) {
+    lista.innerHTML = "";
+    jumento.forEach(function (pessoa) {
+        lista.innerHTML = lista.innerHTML + "<li>" + pessoa.nome + "</li>";
+    }
+    )
+};
+
+function atualizar() {
+    let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa");
+    promessa.then(executar).then(executar2)
 }
 
-let promessa = fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa");
-console.log(promessa);
+function adicionar() {
+    let nomeDaPessoa = nome.value;
+    console.log(nomeDaPessoa);
 
-let promessa2 = promessa.then(executar)
+    let requestPost = {
+        method: "POST",
+        //Ato de fé
+        headers: {
+            "content-type" : "application/json"
+        },
+        //conteudo que eu desejo enviar
+        body: JSON.stringify({
+            idade: 20,
+            nome: nomeDaPessoa,
+        }),
+    };
 
-promessa2.then(executar2)
-
+    fetch("https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa", requestPost).then(function(resposta){
+        console.log(resposta)
+    })
+}
